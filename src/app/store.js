476a8da350d7a55814f16112601c4a96a2366ff6
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-19 23:11:22 +0600

    reducer added on the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
+import authReducer from "./../features/auth/authSlice";
+import conversationsReducer from "./../features/conversations/conversationsSlice";
+import messagesReducer from "./../features/messages/messagesSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    conversations: conversationsReducer,
+    messages: messagesReducer,
   },
 });
