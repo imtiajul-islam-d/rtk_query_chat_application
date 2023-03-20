ad3c3c0da7ad357381c25df51d98e0a638b28e94
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-20 19:01:22 +0600

    async function added on data resolve

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,13 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 // initialState
-const initialState = {};
+const initialState = {
+  accessToken: undefined,
+  user: undefined,
+};
 // slice
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    userLoggedIn: (state, action) => {
+      state.accessToken = action.payload.accessToken;
+      state.user = action.payload.user;
+    },
+    userLoggedOut: (state) => {
+      state.accessToken = undefined;
+      state.user = undefined;
+    },
+  },
 });
 
-export const {} = authSlice.actions;
+export const { userLoggedIn, userLoggedOut } = authSlice.actions;
 export default authSlice.reducer;
