700982c2ef6f2e4e04ebefd38f1188d41640c4ea
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-21 21:42:17 +0600

    correction on inboxContainer

--- src/components/InboxContainer/InboxContainer.js
+++ src/components/InboxContainer/InboxContainer.js
@@ -11,13 +11,15 @@ const InboxContainer = ({ messages = [] }) => {
       <div className="relative w-full p-6 overflow-y-auto">
         <ul className="space-y-2">
           {/* inbox */}
-          {messages?.map((message) => {
-            const { message: lastMessage, id, sender } = message || {};
-            const justify =
-              sender?.email !== email ? "justify-start" : "justify-end";
-            return <Inbox key={id} message={lastMessage} justify={justify} />;
-          })}
-          <Inbox />
+          {messages
+            ?.slice()
+            ?.sort((a, b) => a.timestamp - b.timestamp)
+            ?.map((message) => {
+              const { message: lastMessage, id, sender } = message || {};
+              const justify =
+                sender?.email !== email ? "justify-start" : "justify-end";
+              return <Inbox key={id} message={lastMessage} justify={justify} />;
+            })}
         </ul>
       </div>
     </>
