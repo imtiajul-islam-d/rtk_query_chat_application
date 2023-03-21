d6e70614017d3cf01c6198b0761e90f9eb844a78
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-21 11:52:40 +0600

    logout function added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chat from "./components/Chat/Chat";
 import Inbox from "./components/Inbox/Inbox";
 import Login from "./components/Login/Login";
+import Nav from "./components/Nav/Nav";
 import PrivetRoute from "./components/PrivetRoute/PrivetRoute";
 import PublicRoute from "./components/PublicRoute/PublicRoute";
 import Registration from "./components/Registration/Registration";
@@ -15,42 +16,45 @@ function App() {
   return !authCheck ? (
     <div>Loading...</div>
   ) : (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Registration />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/inbox"
-          element={
-            <PrivetRoute>
-              <Chat />
-            </PrivetRoute>
-          }
-        />
-        <Route
-          path="/inbox/:id"
-          element={
-            <PrivetRoute>
-              <Inbox />
-            </PrivetRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Nav></Nav>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Registration />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/inbox"
+            element={
+              <PrivetRoute>
+                <Chat />
+              </PrivetRoute>
+            }
+          />
+          <Route
+            path="/inbox/:id"
+            element={
+              <PrivetRoute>
+                <Inbox />
+              </PrivetRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { userLoggedOut } from "../../features/auth/authSlice";
 import logo from "./../../assets/logo.svg";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch(userLoggedOut());
+    localStorage.removeItem("auth");
+  };
   return (
     <>
       <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
@@ -10,7 +17,9 @@ const Nav = () => {
             <img className="h-10" src={logo} alt="Logo" />
             <ul>
               <li className="text-white">
-                <a href="#">Logout</a>
+                <span className="cursor-pointer" onClick={logout}>
+                  Logout
+                </span>
               </li>
             </ul>
           </div>
