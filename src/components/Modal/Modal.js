0f63f07a8eec44beba390e8b9b6ea9e11a1c3ba8
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-23 16:13:01 +0600

    conversation update and adding new conversation

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  conversationsApi,
+  useAddConversationMutation,
+  useEditConversationMutation,
+} from "../../features/conversations/conversationsApi";
+import { useGetUserQuery } from "../../features/user/userAPI";
 import isValidEmail from "./../../utils/isValidEmail";
 
 export default function Modal({ open, control }) {
+  const dispatch = useDispatch();
   const [to, setTo] = useState("");
   const [message, setMessage] = useState("");
+  const [conversation, setConversation] = useState(undefined);
+
+  const [userCheck, setUserCheck] = useState(false);
+  const { data: isUserAvailable } = useGetUserQuery(to, {
+    skip: !userCheck,
+  });
+  const { user: loggedInUser } = useSelector((state) => state.auth);
+  const { email: myEmail } = loggedInUser || {};
+
+  //
+  const [addConversation, { isSuccess: isAddConversationSuccess }] =
+    useAddConversationMutation();
+  const [editConversation, { isSuccess: isEditConversationSuccess }] =
+    useEditConversationMutation();
+
+  useEffect(() => {
+    if (isUserAvailable?.length > 0 && isUserAvailable[0]?.email !== myEmail) {
+      dispatch(
+        conversationsApi.endpoints.getConversation.initiate({
+          email: myEmail,
+          participantEmail: to,
+        })
+      )
+        .unwrap()
+        .then((data) => {
+          setConversation(data);
+        });
+    }
+  }, [isUserAvailable, myEmail, dispatch, to]);
+
+  const doSearch = (value) => {
+    if (isValidEmail(value)) {
+      setTo(value);
+      setUserCheck(true);
+      console.log("check");
+    }
+  };
   const debounceHandler = (fn, delay) => {
     let timeOutId;
     return (...args) => {
@@ -13,13 +58,33 @@ export default function Modal({ open, control }) {
       }, delay);
     };
   };
-  const doSearch = (value) => {
-    if (isValidEmail(value)) {
-      setTo(value);
-      console.log("check");
-    }
-  };
   const handleSearch = debounceHandler(doSearch, 500);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (conversation?.length > 0) {
+      // edit
+      editConversation({
+        id: conversation[0]?.id,
+        data: {
+          participants: `${myEmail}-${isUserAvailable[0]?.email}`,
+          users: [loggedInUser, isUserAvailable[0]],
+          message,
+          timestamp: new Date().getTime(),
+        },
+      });
+    }
+    if (conversation?.length === 0) {
+      // add
+      addConversation({
+        participants: `${myEmail}-${isUserAvailable[0]?.email}`,
+        users: [loggedInUser, isUserAvailable[0]],
+        message,
+        timestamp: new Date().getTime(),
+      });
+    }
+    console.log("object");
+  };
   return (
     open && (
       <>
@@ -31,7 +96,7 @@ export default function Modal({ open, control }) {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Send message
           </h2>
-          <form className="mt-8 space-y-6">
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
             <input type="hidden" name="remember" value="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -59,6 +124,7 @@ export default function Modal({ open, control }) {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                   placeholder="Message"
+                  onChange={(e) => setMessage(e.target.value)}
                 />
               </div>
             </div>
@@ -67,12 +133,20 @@ export default function Modal({ open, control }) {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                disabled={conversation === undefined}
               >
                 Send Message
               </button>
             </div>
-
-            {/* <Error message="There was an error" /> */}
+            {isUserAvailable?.length === 0 && (
+              <p className="text-red-500 text-sm">This user does not exist</p>
+            )}
+            {isUserAvailable?.length > 0 &&
+              isUserAvailable[0]?.email === myEmail && (
+                <p className="text-red-500 text-sm">
+                  Sorry! you can not send message to yourself!!
+                </p>
+              )}
           </form>
         </div>
       </>
